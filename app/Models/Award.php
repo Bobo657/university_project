fb82deadc6d667e6ecf5298e09291e899c392d38
Bobo657 <47561918+Bobo657@users.noreply.github.com>
2023-02-06 14:15:57 +0100

    user panel fixed

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -17,13 +17,13 @@ class Award extends Model
 
     public function votes()
     {
-        // return  $this->hasManyThrough(
-        //     Vote::class,
-        //     Contestant::class,
-        //     'contestantable_id',
-        //     'contestant_id',
-        //     'id',
-        //     'id'
-        // )->where('contestants.contestantable_type', 'award');
+        return  $this->hasManyThrough(
+            Vote::class,
+            Contestant::class,
+            'contestantable_id',
+            'contestant_id',
+            'id',
+            'id'
+        )->where('contestants.contestantable_type', 'award');
     }
 }
